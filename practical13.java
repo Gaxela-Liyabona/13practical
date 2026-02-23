24b786d92c79e3e40c435e282a09bd9b1c98d1f1
Gaxela-Liyabona <4544941@myuwc.ac.za>
2026-02-23 13:16:18 +0000

    I imported java packages that I'll need and created Node class to store record from the file, and on main class coded  try-catch

---
+++ practical13.java
@@ -0,0 +1,24 @@
+//Start of the programm
+import java.lang.Math.*;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+  public class practical13{
+     static class Node{ // I'll use this class to store each recorf from ulysses file 
+      int key;
+      String Data;
+      Node(int k,String d){
+        key=k;
+        Data=d;
+      }
+      
+     }
+     public static void main(String[] args) {
+      //Since Im working with an file(externa ulysses.numbered) I'll use the try catch statement so as to ensure that my program runs succesful even in event of an unexpected error (exception)
+       try {
+        
+       } catch (Exception e) {
+        
+       }
+     }
+  }
