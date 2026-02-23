07012511491109acef86d9af6e716324bdc2b905
Gaxela-Liyabona <4544941@myuwc.ac.za>
2026-02-23 13:46:30 +0000

    Added new lines of code cluding code for timing linear and binary searches

--- practical13.java
+++ practical13.java
@@ -23,8 +23,33 @@ import java.util.*;
                 int key = Integer.parseInt(parts[0]);
                 String data = parts[1];
                 list.add(new Node(key, data));
+        } br.close();
+        Node [] node=list.toArray(new Node [0]);//Im preparing to apply the Binary search
+        Random any=new Random();
+        int[] keys=new int[30]; //Array of 30 random keys
+        for (int i =0;i<=30;i++) {
+          keys[i]=any.nextInt(32654)+1;
+
+           long[] linearTimes = new long[30];// Here I time linear search
+            for (int x = 0; x< 30; x++) {
+                long start = System.nanoTime();
+                linearSearch(nodes, keys[x]);
+                long end = System.nanoTime();
+                linearTimes[x] = end - start;
+            }
+
+             long[] binaryTimes = new long[30]; // Here its timing for binary search 
+            for (int m= 0; m < 30; m++) {
+                long start = System.nanoTime();
+                binarySearch(nodes, keys[m]);
+                long end = System.nanoTime();
+                binaryTimes[m] = end - start;
+            }
+
+
+
         }
-        
+
        } catch (Exception e) {
         System.out.println("There's Exception caught");
        }
