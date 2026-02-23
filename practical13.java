e3657b878eea1008d362314b9a3b67de43cab564
Gaxela-Liyabona <4544941@myuwc.ac.za>
2026-02-23 13:29:38 +0000

    New added lines of code

--- practical13.java
+++ practical13.java
@@ -14,11 +14,19 @@ import java.util.*;
       
      }
      public static void main(String[] args) {
-      //Since Im working with an file(externa ulysses.numbered) I'll use the try catch statement so as to ensure that my program runs succesful even in event of an unexpected error (exception)
+      //Since Im working with an file(external ulysses.numbered) I'll use the try catch statement so as to ensure that my program runs succesful even in event of an unexpected error (exception)
        try {
+        BufferedReader read= new BufferedReader(new FileReader("ulysses.numbered"));
+        List<Node>list=new ArrayList<>();
+        while ((line = br.readLine()) != null) {
+                String[] parts = line.split(" ", 2);
+                int key = Integer.parseInt(parts[0]);
+                String data = parts[1];
+                list.add(new Node(key, data));
+        }
         
        } catch (Exception e) {
-        
+        System.out.println("There's Exception caught");
        }
      }
   }
